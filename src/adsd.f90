ff4a1d598f74c85e301f393c46379b172cea2b52
rightfold <rightfold@gmail.com>
2017-02-26 19:23:03 +0100

    Add dummy operations on dummy data to main program

--- src/adsd.f90
+++ src/adsd.f90
@@ -1,7 +1,29 @@
 PROGRAM adsd
 
+    USE ads_sys
+
     IMPLICIT NONE
 
-    PRINT *, 'Hello, world!'
+    REAL, DIMENSION(25) :: input
+    REAL, DIMENSION(5) :: buffer
+    REAL :: interval, now, value
+    INTEGER :: i, j
+
+    interval = size(buffer)
+
+    DO i = 1, size(input)
+        input(i) = i
+    END DO
+
+    DO i = 1, size(input), int(interval)
+        buffer = 0
+        DO j = 1, int(interval)
+            now = j - 1
+            value = input(i + j - 1)
+            CALL feed(buffer, interval, now, value)
+            PRINT *, i + j - 1, j, full(size(buffer), interval, now + 1), buffer
+        END DO
+        PRINT *
+    END DO
 
 END PROGRAM
