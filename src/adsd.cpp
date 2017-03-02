29166fa183422dd4c45712e2217426fbcabdf12d
rightfold <rightfold@gmail.com>
2017-03-02 19:29:10 +0100

    Receive channel and value as separate message parts

--- src/adsd.cpp
+++ src/adsd.cpp
@@ -7,11 +7,26 @@ int main() {
     zmq::socket_t pull(context, ZMQ_PULL);
     pull.bind("tcp://0.0.0.0:1337");
 
+    zmq::message_t channel_message;
+    zmq::message_t value_message;
     for (;;) {
-        zmq::message_t message;
-        pull.recv(&message);
+        pull.recv(&channel_message);
+        if (!channel_message.more()) {
+            std::cerr << "expecting more\n";
+            continue;
+        }
 
-        std::cout << "Received message: " << message.size() << '\n';
+        pull.recv(&value_message);
+        if (value_message.more()) {
+            std::cerr << "not expecting more\n";
+            continue;
+        }
+        if (value_message.size() != 8) {
+            std::cerr << "expecting eight-byte value\n";
+            continue;
+        }
+
+        std::cout << channel_message.data() << " " << value_message.data() << '\n';
     }
 
     return 0;
