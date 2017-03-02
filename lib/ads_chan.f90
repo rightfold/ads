24f9704d7a368bfd0a8e3e75cb99aaa28dfdad51
rightfold <rightfold@gmail.com>
2017-03-02 19:43:19 +0100

    Inline bin

--- lib/ads_chan.f90
+++ lib/ads_chan.f90
@@ -2,16 +2,7 @@ MODULE ads_chan
 
     IMPLICIT NONE
 
-    PRIVATE :: bin
-
 CONTAINS
-
-    PURE FUNCTION bin(size, interval, now)
-        INTEGER, INTENT(IN) :: size
-        REAL, INTENT(IN) :: interval, now
-        INTEGER :: bin
-        bin = 1 + int(size / interval * now)
-    END FUNCTION
 
     PURE FUNCTION buffer_index(interval, now)
         REAL, INTENT(IN) :: interval, now
@@ -22,8 +13,10 @@ CONTAINS
     SUBROUTINE feed(buffer, interval, now, value)
         REAL, INTENT(INOUT), DIMENSION(:) :: buffer
         REAL, INTENT(IN) :: interval, now, value
+        INTEGER :: bin
         IF (0 .LE. now .AND. now .LT. interval) THEN
-            buffer(bin(size(buffer), interval, now)) = value
+            bin = 1 + int(size(buffer) / interval * now)
+            buffer(bin) = value
         END IF
     END SUBROUTINE
 
